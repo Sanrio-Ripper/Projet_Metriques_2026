d903ba183746cf87663ec1161233912d7e9b7fd4
Sanrio-Ripper <sabrou26022004@gmail.com>
2026-05-18 15:50:37 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import requests
-from flask import Flask, jsonify, render_template, jsonify
+from flask import Flask, jsonify, render_template
 
 app = Flask(__name__)
 
@@ -8,28 +8,30 @@ def hello_world():
     return render_template('hello.html')
 
 # Déposez votre code à partir d'ici :
+
 @app.route("/contact")
 def MaPremiereAPI():
     return "<h2>Ma page de contact</h2>"
-
-# Ne rien mettre après ce commentaire
-    
-if __name__ == "__main__":
-  app.run(host="0.0.0.0", port=5000, debug=True)
 
 @app.get("/paris")
 def api_paris():
     url = "https://api.open-meteo.com/v1/forecast?latitude=48.8566&longitude=2.3522&hourly=temperature_2m"
     response = requests.get(url)
     data = response.json()
-
     times = data.get("hourly", {}).get("time", [])
     temps = data.get("hourly", {}).get("temperature_2m", [])
-
     n = min(len(times), len(temps))
     result = [
         {"datetime": times[i], "temperature_c": temps[i]}
         for i in range(n)
     ]
-
     return jsonify(result)
+
+@app.route("/rapport")
+def mongraphique():
+    return render_template("graphique.html")
+
+# Ne rien mettre après ce commentaire
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
